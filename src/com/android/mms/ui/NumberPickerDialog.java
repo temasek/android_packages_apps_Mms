20496f6387524d2d8c448bcbc65e7c39c7cb1237
Janson Kang <temasek71@gmail.com>
2014-01-24 19:45:15 +0800

    Fix build

--- src/com/android/mms/ui/NumberPickerDialog.java
+++ src/com/android/mms/ui/NumberPickerDialog.java
@@ -20,6 +20,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
